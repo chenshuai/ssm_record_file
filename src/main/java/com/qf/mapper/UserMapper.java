ec5dbda0bdd80ea78b38bb8a455d029a7313828e
chris <930193463@qq.com>
2019-07-12 19:17:13 +0800

    Initial commit

---
+++ src/main/java/com/qf/mapper/UserMapper.java
@@ -0,0 +1,16 @@
+package com.qf.mapper;
+
+
+import com.qf.pojo.User;
+
+public interface UserMapper {
+
+    User getUser(User user);
+
+    int addUser(User user);
+
+    String getUsername(String username);
+
+    String getPassword(String username);
+
+}
